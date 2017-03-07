16c3890fb9283149cd99760d401cbef09df73789
Keks <keks@htmlacademy.ru>
2017-03-07 19:53:45 +0300

    :kissing_cat: Добавил сборку sass в grunt

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-sass");
+
+  grunt.initConfig({
+    sass: {
+      style: {
+        files: {
+          "css/style.css": "sass/style.scss"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 2 versions"
+            ]})
+          ]
+        },
+        src: "css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["sass/**/*.{scss,sass}"],
+        tasks: ["sass", "postcss"]
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+};
